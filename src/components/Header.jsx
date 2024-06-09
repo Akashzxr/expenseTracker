e16efd9ee269d3ad4a1ae05e28e4e9761017ea49
akashks <akashks8855@gmail.com>
2024-06-10 00:57:33 +0530

    first commit - created header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+function Header(){
+    return (
+     <div className="w-full h-28 bg-violet-100">
+        <h1 className="text-blue-500 font-bold text-4xl text-center pt-9">EXPENSE TRACKER</h1>
+     </div>
+    );
+}
+
+export default Header;
